98e1f1d375d13a4265fdeaf7642380089793ece5
Eduardo França <edox86@gmail.com>
2021-03-13 15:21:13 -0300

    Initial commit
    Laravel 8 CRUD Tutorial. Model and Controller added, main layout index,
    create and store pages added.

---
+++ laravel/resources/views/products/index.blade.php
@@ -0,0 +1,64 @@
+
+@extends('produts.laytouts')
+
+@section('content')
+  <div class="row">
+    <div class="col-lg-12 margin-tb">
+      <div class="pull-left">
+        <h2>Laravel 8 CRUD Tutorial</h2>
+      </div>
+      <div class="pull-right">
+        <a class="btn btn-success" href="{{ route('products.create') }}">
+          Add New Product
+        </a>
+      </div>
+    </div>
+  </div>
+
+  @if ($message = $session::get('success'))
+    <div class="alert alert-success">
+      <p>{{ $message }}</p>
+    </div>
+  @endif
+
+  <table class="table table-bordered">
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Details</th>
+      <th width="280px">Action</th>
+    </tr>
+
+    @foreach ($products as $product)
+      <tr>
+        <td>{{ $product->id }}</td>
+        <td>{{ $product->name }}</td>
+        <td>{{ $product->detail }}</td>
+        <td> <!-- begin actions -->
+          <form 
+            action="{{ route('products.destroy', $product->id) }}"
+            method="POST"
+          >
+            <a 
+              class="btn btn-info" 
+              href="{{ route('products.show', $product->id) }}"
+            > Show </a>
+
+            <a
+              class="btn btn-primary"
+              href="{{ route('products.edit', $product->id) }}"
+            > Edit </a>
+      
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">
+          </form>
+        </td> <!-- end actions --> 
+      </tr>
+    @endforeach
+  </table>
+  
+  {!! $products->links() !!}
+
+@endsection
+
